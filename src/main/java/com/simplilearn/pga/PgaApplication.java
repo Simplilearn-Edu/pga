818f36bd1a786295f700f81ed83e3995ad5200df
waseem <waseem_6957@yahoo.com>
2023-01-12 10:28:04 +0530

    Initial commit

---
+++ src/main/java/com/simplilearn/pga/PgaApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.pga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplilearn/pga/PgaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.pga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
